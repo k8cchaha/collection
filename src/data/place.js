e97dd82bd3fa8ed7f43bf78c4b3744f9a6044b33
Alex <alex.wang@gofreight.com>
2024-05-21 02:53:29 +0800

    update lego view

---
+++ src/data/place.js
@@ -0,0 +1,4 @@
+export const placeList = [
+  { name: '街景 + 大型建築', imgUrl: './imgs/lego/place/building.jpg' },
+  { name: '忍者大型 + 動畫', imgUrl: './imgs/lego/place/ninja_animate.jpg'}
+]
--- src/views/LegoView.vue
+++ src/views/LegoView.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <h1>LEGO</h1>
-    <div class="number-row">
+    <!-- <h1>LEGO</h1> -->
+    <!-- <div class="number-row">
       <label for="photoCount">Photos : </label>
       <select v-model="photosPerRow" id="photoCount">
         <option value="1">1</option>
@@ -11,28 +11,63 @@
         <option value="5">5</option>
         <option value="6">6</option>
       </select>
+    </div> -->
+    <div class="filter">
+      <div>
+        <button type="button" :class="{ select: mode === 'place' }" @click="switchMode('place')">場景模式</button>
+        <button type="button" :class="{ select: mode === 'data' }" @click="switchMode('data')">資料模式</button>
+      </div>
+      <div v-if="mode === 'place'">
+        <div class="dropdown-set">
+          <div class="dropdown-title">場景：</div>
+          <Dropdown :list="placeList" @updateSelectPlace="updateSelectPlace"/>
+        </div>
+      </div>
+      <div v-if="mode === 'data'">
+        
+      </div>
     </div>
-    <div class="photo-grid" :style="{ gridTemplateColumns: `repeat(${photosPerRow}, 1fr)` }">
+    <div v-show="mode === 'place'">
+      <div class="content">
+        <div class="main-pic">
+          <img :src="selectedPlace.imgUrl" />
+        </div>
+        <div class="gap"></div>
+        <div class="sub-pic">
+          <div >
+            圖片中盒組
+          </div>
+
+        </div>
+      </div>
+    </div>
+    <div v-show="mode === 'data'" class="photo-grid" :style="{ gridTemplateColumns: `repeat(${photosPerRow}, 1fr)` }">
       <LegoItem v-for="item in legoList" :key="item.id" :item="item" />
     </div>
   </div>
 </template>
 
 <script>
-import LegoItem from '../components/LegoItem.vue'
+import LegoItem from '../components/LegoItem.vue';
+import Dropdown from '../components/Dropdown.vue';
 import { mapState } from 'pinia';
 import { useDeviceStore } from '../stores/useDeviceStore';
 import { legoList } from '../data/lego.js';
+import { placeList } from '../data/place.js';
 
 export default {
   name: 'LegoView',
   components: {
-    LegoItem
+    LegoItem,
+    Dropdown
   },
   data() {
     return {
+      mode: 'place',
       photosPerRow: 3,
       legoList: legoList,
+      placeList: placeList,
+      selectedPlace: placeList[0],
       tempPerRow: 0
     };
   },
@@ -52,10 +87,47 @@ export default {
       immediate: true,
     }
   },
+  methods: {
+    switchMode(mode){
+      this.mode = mode;
+    },
+    updateSelectPlace(val) {
+      this.selectedPlace = val
+      console.log(this.selectedPlace)
+    }
+  }
 }
 </script>
 
 <style scoped>
+.filter {
+  margin-top: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.filter button {
+  border-radius: 0;
+  border: 1px var(--secondary-color) solid;
+  color: var(--secondary-color);
+  background-color: white;
+}
+.filter button:hover, .filter button.select {
+  background-color: var(--secondary-color);
+  color: white;
+}
+.dropdown-set {
+  display: flex;
+  align-items: center;
+}
+.dropdown-title {
+  height: 38px;
+  line-height: 38px;
+  background-color: white;
+  border: 1px solid var(--primary-color);
+  border-radius: 5px 0 0 5px ;
+  padding: 0 10px 0 20px;
+}
 .photo-grid {
   display: grid;
   gap: 16px;
@@ -66,6 +138,26 @@ export default {
   position: absolute;
     right: 20px;
     top: 80px;
+}
+.content {
+  width: 100%;
+  margin-top: 10px;
+  display: flex;
+  align-items: stretch;
+}
+.main-pic {
+  flex: 0 0 70%;
+}
+.main-pic img {
+  max-width: 100%;
+  vertical-align: middle;
+}
+.gap {
+  flex: 0 0 2%;
+}
+.sub-pic {
+  flex: 0 0 28%;
+
 }
 
 @media (max-width: 768px) {
